8750dee1d98d7e0faec54d535f416708bc67cef4
周兆雄 <1796822664@qq.com>
2023-08-08 15:05:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 终端门店自动分类打标
+标签体系为5.0
+## 数据表standard_db.di_store_classify_dedupe  
+当前为测试用表，标签字段catecory1_new
---
+++ label_lstm/global_parameter.py
@@ -0,0 +1,19 @@
+class StaticParameter:
+    # 本地调试参数
+    # 设置最频繁使用的词个数(在texts_to_matrix取前MAX_NB_WORDS列)
+    MAX_WORDS_NUM = 120000
+    # 每条cut_name最大的长度
+    MAX_LENGTH = 7
+    # 设置Embedding层的维度
+    EMBEDDING_DIM = 120
+    # 全国数据分段跑
+    SEGMENT_NUMBER = 8
+    # 抽样数量
+    DATA_NUMBER = 50000
+    # 标准广州数据
+    PATH_TRAIN = 'standard_store_'
+    # Linux服务器参数
+    PATH_ZZX = '/home/data/temp/zhouzx'
+    PATH_ZZX_DATA = '/home/data/temp/zhouzx/data/test/'
+    PATH_ZZX_STANDARD_DATA = '/home/data/temp/zhouzx/standard_data/test/'
+    PATH_ZZX_PREDICT_DATA = '/home/data/temp/zhouzx/predict_data/test/'
